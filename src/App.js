61a722db64837f81e065350694afd37d0f0a2905
Aniket Chitransh <ani.chitransh@gmail.com>
2024-02-02 09:51:03 +0530

    Starting with the app.js project file

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-import React from 'react'
+import React, { useEffect } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { FiSettings } from 'react-icons/fi';
+import { TooltipComponent } from '@syncfusion/ej2-react-popups';
 
 import './App.css'
 
 const App = () => {
     return (
-        <h1 className='underline text-3xl'>App</h1>
+        <>
+            <BrowserRouter>
+                <div className="flex relative dark:bg-main-dark-bg">
+                    <div className="fixed ring-4 bottom-4" style={{ zIndex: '1000' }}>
+
+                    </div>
+                </div>
+            </BrowserRouter>
+        </>
     )
 }
 
