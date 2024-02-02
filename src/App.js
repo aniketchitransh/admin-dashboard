7ac91cf62b38b027177861710c5fee8c8b7b6351
Aniket Chitransh <ani.chitransh@gmail.com>
2024-02-02 10:19:41 +0530

    Created a place for Navbar

--- src/App.js
+++ src/App.js
@@ -24,16 +24,23 @@ const App = () => {
                         </TooltipComponent>
                     </div>
                     {activeMenu ? (
-                        <div>
+                        <div className='w-72 fixed sidebar dark:bg-secondary-dark-bg bg-white'>
                             Sidebar
                         </div>
                     ) : (
-                        <div>
+                        <div className='w-0 dark:bg-secondary-dark-bg'>
                             Sidebar w-0
                         </div>
                     )}
+                    <div className={
+                        `dark:bg-main-bg bg-main-bg min-h-screen md:ml-72 w-full ${activeMenu ? 'md:ml-72' : 'flex-2'}`
+                    }>
+                        <div className="fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full">
+                            Navbar
+                        </div>
+                    </div>
                 </div>
-            </BrowserRouter>
+            </BrowserRouter >
         </>
     )
 }
