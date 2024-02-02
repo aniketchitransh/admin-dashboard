5abe461730db63945d1ebfe31ff0923489adb680
Aniket Chitransh <ani.chitransh@gmail.com>
2024-02-02 10:07:38 +0530

    Added a settings icons and moving on to Sidebar

--- src/App.js
+++ src/App.js
@@ -6,13 +6,32 @@ import { TooltipComponent } from '@syncfusion/ej2-react-popups';
 import './App.css'
 
 const App = () => {
+    const activeMenu = true;
+
     return (
         <>
             <BrowserRouter>
                 <div className="flex relative dark:bg-main-dark-bg">
-                    <div className="fixed ring-4 bottom-4" style={{ zIndex: '1000' }}>
-
+                    <div className="fixed right-4 bottom-4" style={{ zIndex: '1000' }}>
+                        <TooltipComponent content='Settings' position='Top'>
+                            <button
+                                type='button'
+                                className='text-3xl p-3 hover:drop-shadow-xl hover:bg-light-gray text-white'
+                                style={{ background: 'blue', borderRadius: '50%' }}
+                            >
+                                <FiSettings />
+                            </button>
+                        </TooltipComponent>
                     </div>
+                    {activeMenu ? (
+                        <div>
+                            Sidebar
+                        </div>
+                    ) : (
+                        <div>
+                            Sidebar w-0
+                        </div>
+                    )}
                 </div>
             </BrowserRouter>
         </>
