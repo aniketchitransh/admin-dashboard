694059b280d49bbe59b66804f9413008353193ee
Aniket Chitransh <ani.chitransh@gmail.com>
2024-02-09 21:46:29 +0530

    set the active link and normal link type

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -10,7 +10,7 @@ const Sidebar = () => {
     const activeMenu = true;
 
     const activeLink = 'flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-white text-md m-2';
-    const normalLink = 'flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-white text-md m-2';
+    const normalLink = 'flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-gray-700 text-md dark:text-gray-200 dark:hover:text-black hover:bg-light-gray m-2';
 
     return (
         <div className='ml-34 h-screen md:overflow-hidden overflow-auto md:hover:overflow-auto pb-10'>
@@ -45,9 +45,14 @@ const Sidebar = () => {
                                     to={`/${Link.name}`}
                                     key={Link.name}
                                     onClick={() => { }}
-                                    className={({ isActive }) => { }}
+                                    className={({ isActive }) => 
+                                        isActive ? activeLink : normalLink
+                                    }
                                 >
-
+                                    {Link.icon}
+                                    <span className='capitalize'>
+                                        {Link.name}
+                                    </span>
                                 </NavLink>
                             ))}
                         </div>
