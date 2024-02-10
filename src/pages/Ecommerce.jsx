dac509031c846cb14ed123fd0b5d1da6f93209c2
Aniket Chitransh <ani.chitransh@gmail.com>
2024-02-10 14:09:18 +0530

    Created the layout for the ecommerce page and moving to the button component

--- src/pages/Ecommerce.jsx
+++ src/pages/Ecommerce.jsx
@@ -10,7 +10,22 @@ const Ecommerce = () => {
     return (
         <div className='mt-12'>
             <div className='flex flex-wrap lg:flex-nowrap justify-center'>
-
+                <div className='bg-white dark:text-gray-200 dark:bg-secondary-dark-bg h-44 rounded-xl w-full lg:w-80 p-8 pt-9 m-3 bg-gradient-to-r from-indigo-500 to-cyan-500 bg-cover'>
+                    <div className='flex justify-between items-center'>
+                        <div>
+                            <p className='font-bold text-gray-400'>Earnings</p>
+                            <p className='text-2xl'>₹63,448.50</p>
+                        </div>
+                    </div>
+                    <div className='mt-6'>
+                        <Button
+                            color='white'
+                            bgColor='blue'
+                            text='Download'
+                            borderRadius='10px'
+                        />
+                    </div>
+                </div>
             </div>
         </div>
     )
