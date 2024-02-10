14a53b8956e6c66e8b1ad72938749655e22db638
Aniket Chitransh <ani.chitransh@gmail.com>
2024-02-10 12:21:02 +0530

    Starting with some of the imports in the Ecommerce page as  it is the Homepage

--- src/pages/Ecommerce.jsx
+++ src/pages/Ecommerce.jsx
@@ -1,4 +1,9 @@
-import React from 'react'
+import React from 'react';
+// import { GoPrimitiveDot } from 'react-icons/go';
+import { BsCurrencyDollar } from 'react-icons/bs';
+
+import { Stacked, Pie, Button, SparkLine } from '../components';
+import { earningData, SparklineAreaData, ecomPieChartData } from '../data/dummy';
 
 const Ecommerce = () => {
     return (
