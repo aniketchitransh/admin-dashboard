04671ce82bcfada8ca7ef1ea4800a45ba3e2e4f7
Aniket Chitransh <ani.chitransh@gmail.com>
2024-02-11 21:10:12 +0530

    Designing the ThemeSettings component sidebar

--- src/components/ThemeSettings.jsx
+++ src/components/ThemeSettings.jsx
@@ -8,7 +8,13 @@ import { useStateContext } from '../contexts/ContextProvider';
 
 const ThemeSettings = () => {
     return (
-        <div>ThemeSettings</div>
+        <div className='w-screen bg-half-transparent fixed nav-item top-0 right-0'>
+            <div className='float-right h-screen dark:text-gray-200 bg-white dark:[#484B52] w-400'>
+                <div className='flex justify-between items-center p-4 ml-4'>
+                    <p className='text-lg font-semibold'>Settings</p>
+                </div>
+            </div>
+        </div>
     )
 }
 
