b571946a311c2e82274f5a5b1f7d1bf6ff04be3d
Aniket Chitransh <ani.chitransh@gmail.com>
2024-02-11 22:27:16 +0530

    Empliminted dark mode settings to the application

--- src/App.js
+++ src/App.js
@@ -12,10 +12,10 @@ import './App.css'
 
 
 const App = () => {
-    const { activeMenu, themeSettings, setThemeSettings, currentColor } = useStateContext();
+    const { activeMenu, themeSettings, setThemeSettings, currentColor, currentMode } = useStateContext();
 
     return (
-        <>
+        <div className={currentMode === 'Dark' ? 'dark' : ''}>
             <BrowserRouter>
                 <div className="flex relative dark:bg-main-dark-bg">
                     <div className="fixed right-4 bottom-4" style={{ zIndex: '1000' }}>
@@ -40,7 +40,7 @@ const App = () => {
                         </div>
                     )}
                     <div className={
-                        `dark:bg-main-bg bg-main-bg min-h-screen w-full ${activeMenu ? 'md:ml-72' : 'flex-2'}`
+                        `dark:bg-main-dark-bg bg-main-bg min-h-screen w-full ${activeMenu ? 'md:ml-72' : 'flex-2'}`
                     }>
                         <div className="fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full">
                             <Navbar />
@@ -79,7 +79,7 @@ const App = () => {
                     </div>
                 </div>
             </BrowserRouter >
-        </>
+        </div>
     )
 }
 
