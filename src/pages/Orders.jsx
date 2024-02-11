acd19480592c8e78b5f1b0e948bc47c75941521d
Aniket Chitransh <ani.chitransh@gmail.com>
2024-02-11 17:44:48 +0530

    Orders page completed

--- src/pages/Orders.jsx
+++ src/pages/Orders.jsx
@@ -3,17 +3,19 @@ import { GridComponent, ColumnsDirective, ColumnDirective, Resize, Sort, Context
 
 import { ordersData, contextMenuItems, ordersGrid } from '../data/dummy';
 import { Header } from '../components';
+import { excelExport } from '@syncfusion/ej2/pivotview';
 
 const Orders = () => {
   return (
     <div className='m-2 md:m-10 p-2 md:p-10 bg-white rounded-3xl'>
       <Header category='Page' title='Orders' />
-      <GridComponent id='gridcomp' dataSource={ordersData}>
+      <GridComponent id='gridcomp' dataSource={ordersData} allowPaging allowSorting>
         <ColumnsDirective>
           {ordersGrid.map((item, index) => (
             <ColumnDirective key={index} {...item} />
           ))}
         </ColumnsDirective>
+        <Inject services={[Resize, Sort, Page, ContextMenu, Filter, ExcelExport, PdfExport]} />
       </GridComponent>
     </div>
   )
