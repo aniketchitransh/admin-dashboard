6c00971e19eda572a9d47d0b5ad95869e120be03
Aniket Chitransh <ani.chitransh@gmail.com>
2024-02-11 18:02:34 +0530

    Starting with customers page

--- src/pages/Customers.jsx
+++ src/pages/Customers.jsx
@@ -1,4 +1,7 @@
-import React from 'react'
+import React from 'react';
+import { GridComponent, ColumnDirective, ColumnsDirective, Page, Inject, Selection, Edit, Toolbar, Sort, Filter } from '@syncfusion/ej2-react-grids';
+
+import { customersData, customersGrid } from '../data/dummy';
 
 const Customers = () => {
     return (
