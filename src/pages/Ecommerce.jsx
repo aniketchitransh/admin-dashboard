086e0f56000fd05459f993f669fc979454df2f30
Aniket Chitransh <ani.chitransh@gmail.com>
2024-02-11 13:25:28 +0530

    Making cards in ecommerce below hero section

--- src/pages/Ecommerce.jsx
+++ src/pages/Ecommerce.jsx
@@ -10,7 +10,7 @@ const Ecommerce = () => {
     return (
         <div className='mt-12'>
             <div className='flex flex-wrap lg:flex-nowrap justify-center'>
-                <div className='grow bg-white dark:text-gray-200 dark:bg-secondary-dark-bg h-44 rounded-xl w-full lg:w-80 p-8 pt-9 m-3 bg-gradient-to-r from-[#C9FFBF] to-[#FFAFBD]'>
+                <div className='bg-white dark:text-gray-200 dark:bg-secondary-dark-bg h-44 rounded-xl w-full lg:w-80 p-8 pt-9 m-3 bg-gradient-to-r from-[#C9FFBF] to-[#FFAFBD]'>
                     <div className='flex justify-between items-center'>
                         <div>
                             <p className='font-bold text-gray-400'>Earnings</p>
@@ -27,8 +27,32 @@ const Ecommerce = () => {
                         />
                     </div>
                 </div>
-                <div className=''>
-
+                <div className='flex m-3 flex-wrap justify-center items-center gap-1'>
+                    {earningData.map((item) => (
+                        <div
+                            key={item.title}
+                            className='bg-white dark:text-gray-200 dark:bg-secondary-dark-bg md:w-56 p-4 pt-9 rounded-2xl'
+                        >
+                            <button
+                                type='button'
+                                style={{ color: item.iconColor, backgroundColor: item.iconBg }}
+                                className='text-2xl opasity-0.9 rounded-full p-4 hover:drop-shadow-xl'
+                            >
+                                {item.icon}
+                            </button>
+                            <p className='mt-3'>
+                                <span className='text-xl font-semibold'>
+                                    {item.amount}
+                                </span>
+                                <span className={`text-sm text-${item.pcColor} ml-2`}>
+                                    {item.percentage}
+                                </span>
+                            </p>
+                            <p className='text-sm text-gray-400 mt-1'>
+                                {item.title}
+                            </p>
+                        </div>
+                    ))}
                 </div>
             </div>
         </div>
