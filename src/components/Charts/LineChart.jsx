49836f4a4e11a52a94a0afdde2da1dc0dd92548b
Aniket Chitransh <ani.chitransh@gmail.com>
2024-02-12 11:48:06 +0530

    Mapped the data from dummy to the line page

--- src/components/Charts/LineChart.jsx
+++ src/components/Charts/LineChart.jsx
@@ -1,8 +1,16 @@
-import React from 'react'
+import React from 'react';
+import { ChartComponent, SeriesCollectionDirective, SeriesDirective, Inject, LineSeries, Legend, DateTime, Tooltip } from '@syncfusion/ej2-react-charts'
+
+import { lineCustomSeries, LinePrimaryXAxis, LinePrimaryYAxis } from '../../data/dummy';
 
 const LineChart = () => {
     return (
-        <div>LineChart</div>
+        <ChartComponent>
+            <Inject services={[LineSeries, DateTime, Legend, Tooltip]} />
+            <SeriesCollectionDirective>
+                {lineCustomSeries.map((item, index) => <SeriesDirective key={index} {...item} />)}
+            </SeriesCollectionDirective>
+        </ChartComponent>
     )
 }
 
